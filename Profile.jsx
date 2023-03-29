dfdb21367131b18fef8ad5689157c519c06b4fca
kimtigithub <123611210+kimtigithub@users.noreply.github.com>
2023-03-29 14:38:27 +0700

    Add files via upload

---
+++ Profile.jsx
@@ -0,0 +1,12 @@
+import "./Profile.css";
+function Profile() {
+  return (
+    <div className="layout">
+      <img src="assets/photo-1611221911930-7a9d39cb219b.jpg" alt="profile" />
+      
+      <h1 className="name">Betty White</h1>
+      <hr></hr>
+    </div>
+  );
+}
+export default Profile;
